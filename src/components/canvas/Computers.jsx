216276a1b9c71bd7aa6dfac91b9734e0ea3e0285
obokhai <aibormoses1997@gmail.com>
2023-08-01 11:13:24 +0100

    updated the computer.jsx 3d model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,30 +1,28 @@
-import { Suspense, useEffect, useState } from "react";
-
+import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-const Computers = ({isMobile}) => {
-  const computer = useGLTF('./desktop_pc/scene.gltf');
+const Computers = ({ isMobile }) => {
+  const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
     <mesh>
-    <hemisphereLight intensity={3.0} groundColor='black' />
-    <pointLight intensity={1} /> 
-       
-       <spotLight
-        position={[-20,50,10]}
+      <hemisphereLight intensity={0.15} groundColor='black' />
+      <spotLight
+        position={[-20, 50, 10]}
         angle={0.12}
         penumbra={1}
         intensity={1}
         castShadow
         shadow-mapSize={1024}
-       />
+      />
+      <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.6 : 0.65}
-        position={isMobile?[0,-2.4,-2.2]:[0, -2.7, -1.5]}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -32,32 +30,33 @@ const Computers = ({isMobile}) => {
 };
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
 
-  const[isMobile,setIsMobile] = useState(false)
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
 
-  useEffect(() =>{
-    //Add Listener for changes to the screen size
-    const mediaQuery = window.matchMedia(
-    '(max-width:500px)'
-    )
-    // set the initial value of the `isMobile` state 
-    setIsMobile(mediaQuery.matches)
-    //define a callback function to handle changes to the mediaQuery
-    const handleMediaQueryChange = (event) =>{
-      setIsMobile(event.matches)
-    }
-    //Add the callack function to handle changes to the media query
-    mediaQuery.addEventListener("change",handleMediaQueryChange)
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
 
-    //remove the listener when the component is unmounted
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
     return () => {
-      mediaQuery.removeEventListener("change", handleMediaQueryChange)
-    }
-  },[])
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <Canvas
       frameloop='demand'
-       shadows
+      shadows
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
